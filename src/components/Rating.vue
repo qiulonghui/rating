f0013b13ed5f0bfe15efda3f9a84f519fbfa8f33
Qiulonghui <569374939@qq.com>
2020-07-03 14:10:12 +0800

    feat: 功能基本实现

--- src/components/Rating.vue
+++ src/components/Rating.vue
@@ -1,65 +1,91 @@
 <template>
   <div class="rating">
-    <div
-      class="star-wrap"
-      v-for="n in count"
-      :key="n"
-      @mouseenter="disabled ? null : (curScore = n)"
-      @click="setScore(n)"
-    >
-      <div
-        class="mask"
-        v-show="n <= Math.ceil(curScore)"
-        :style="{
+
+    <symbol id="icon-star-full" viewBox="0 0 1024 1024">
+      <path d="M964.608 424.96L752.64 631.808l50.176 290.816c1.024 8.192-2.048 15.36-8.192 20.48-3.072 3.072-8.192 4.096-12.288 4.096-3.072 0-6.144-1.024-9.216-2.048L512 806.912 250.88 944.128c-7.168 4.096-15.36 3.072-21.504-2.048-6.144-4.096-9.216-12.288-8.192-20.48l50.176-290.816L59.392 424.96c-5.12-5.12-7.168-13.312-5.12-21.504 2.048-7.168 9.216-13.312 16.384-14.336l291.84-40.96L493.568 82.944c3.072-7.168 10.24-11.264 18.432-11.264s15.36 4.096 18.432 11.264L661.504 348.16l291.84 41.984c8.192 1.024 14.336 6.144 16.384 14.336s0 15.36-5.12 20.48z" p-id="1364"></path>
+    </symbol>
+
+    <div class="star-wrap" v-for="n in count" :key="n" @mouseover="onOver(n)" @mouseout="onOut" @click="setScore(n)">
+      <div class="mask" v-show="n <= Math.ceil(curScore)" :style="{
           width: (curScore - n < 0 ? (1 + curScore - n) * 100 : 100) + '%',
-        }"
-      >
-        <Star :size="size" color="#f2b50f" />
+        }">
+        <img class="img" v-if="activeImage" :src="activeImage" :style="customImgStyle">
+        <svg v-else class="icon" :style="{fill:activeColor}" :width="size" :height="size">
+          <use :xlink:href="'#'+iconid"></use>
+        </svg>
       </div>
-      <Star :size="size" class="b-shape" />
+      <img class="img" v-if="inactiveImage" :src="inactiveImage" :style="customImgStyle">
+      <svg v-else class="icon" :style="{fill:inactiveColor}" :width="size" :height="size">
+        <use :xlink:href="'#'+iconid"></use>
+      </svg>
     </div>
   </div>
 </template>
 
 <script>
-import Star from './Star'
-
 export default {
   name: 'Rating',
-  components: {
-    Star,
-  },
+  components: {},
   props: {
-    score: {
-      type: Number,
-      default: 1.8,
+    value: {
+      type: [Number, String],
+      default: 0
     },
     count: {
       type: Number,
-      default: 6,
+      default: 6
     },
     size: {
       type: String,
-      default: '40',
+      default: '40'
     },
     disabled: {
       type: Boolean,
-      default: false,
+      default: false
     },
+    iconid: {
+      type: String,
+      default: 'icon-star-full'
+    },
+    activeColor: {
+      type: String,
+      default: '#1500b0'
+    },
+    inactiveColor: {
+      type: String,
+      default: '#7669d6'
+    },
+    activeImage: {
+      type: String,
+      default: ''
+    },
+    inactiveImage: {
+      type: String,
+      default: ''
+    },
+    customImgStyle: Object
   },
   data() {
     return {
-      curScore: this.score,
+      curScore: this.value
     }
   },
   computed: {},
   created() {},
   methods: {
-    setScore(n) {
-      console.log(1)
-      this.curScore = n
+    onOver(n) {
+      if (n < this.value) return
+      if (!this.disabled) this.curScore = n
     },
-  },
+    onOut() {
+      if (!this.disabled) this.curScore = this.value
+    },
+    setScore(n) {
+      if (this.disabled) return
+      this.$emit('input', n)
+      this.$emit('after-rate', n)
+    }
+  }
 }
 </script>
 
@@ -77,6 +103,10 @@ export default {
     left: 0;
     z-index: 1;
     overflow: hidden;
+  }
+  .img {
+    width: 40px;
+    height: 40px;
   }
 }
 </style>
